b7837d9b823c3c6523e9fc99fd0e920a042ce83f
Promise <mk@Clearlove.com>
2020-09-03 22:45:44 +0800

    把本地工程添加到GIT

---
+++ git-test-project/src/com/clearlove/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.clearlove.git;
+
+/**
+ * @author promise
+ * @date 2020/9/3 - 22:38
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+    }
+}
